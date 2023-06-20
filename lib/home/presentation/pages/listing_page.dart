45c9b73941eb9c5c9c4d0cb76f1b6356bc0a38a7
Gegerout <selimchik06@gmail.com>
2023-06-20 20:16:47 +0300

    home page update

--- lib/home/presentation/pages/listing_page.dart
+++ lib/home/presentation/pages/listing_page.dart
@@ -12,11 +12,18 @@ class ListingPage extends StatelessWidget {
     final images = [
       "assets/images/dest_1.png",
       "assets/images/dest_2.png",
-      "assets/images/dest_3.png",
       "assets/images/dest_4.png",
+      "assets/images/dest_3.png",
       "assets/images/dest_5.png",
       "assets/images/dest_6.png",
       "assets/images/dest_7.png",
+    ];
+
+    final texts = [
+      ["Goa", "4.7"],
+      ["Mahe", "5.0"],
+      ["Krabi", "4.6"],
+      ["Phuket", "4.5"],
     ];
 
     return Scaffold(
@@ -77,24 +84,284 @@ class ListingPage extends StatelessWidget {
                   ),
                 ),
               ),
-      SizedBox(
-          height: 412,
-          child: MasonryGridView.builder(
-                    physics: const NeverScrollableScrollPhysics(),
+              Padding(
+                padding: const EdgeInsets.only(left: 27, right: 27),
+                child: SizedBox(
+                  height: 380,
+                  child: MasonryGridView.builder(
+                      physics: const NeverScrollableScrollPhysics(),
                       gridDelegate:
                           const SliverSimpleGridDelegateWithFixedCrossAxisCount(
                               crossAxisCount: 2),
                       itemCount: 4,
+                      mainAxisSpacing: 13,
+                      crossAxisSpacing: 15,
                       itemBuilder: (context, index) {
-                        return Container(
-                          height: 200,
-                          child: Image.asset(images[index]),
+                        return SizedBox(
+                          child: Stack(
+                            children: [
+                              Align(
+                                alignment: Alignment.center,
+                                child: Image.asset(
+                                  images[index],
+                                ),
+                              ),
+                              Column(
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.only(
+                                        right: 14, top: 8),
+                                    child: Align(
+                                      alignment: Alignment.topRight,
+                                      child: Container(
+                                          width: 30,
+                                          height: 30,
+                                          decoration: BoxDecoration(
+                                              color: Colors.black,
+                                              borderRadius:
+                                                  BorderRadius.circular(30)),
+                                          child: IconButton(
+                                              padding: EdgeInsets.zero,
+                                              onPressed: () {},
+                                              icon: const Icon(
+                                                Icons.favorite,
+                                                color: Color(0xFFFF5757),
+                                                size: 18,
+                                              ))),
+                                    ),
+                                  ),
+                                  SizedBox(height: index == 1 || index == 3 ? 40 : 135),
+                                  Padding(
+                                    padding: const EdgeInsets.only(left: 17),
+                                    child: Align(
+                                      alignment: Alignment.bottomLeft,
+                                        child: Text(texts[index][0],
+                                            style: GoogleFonts.nunito(
+                                                fontWeight: FontWeight.w700,
+                                                fontSize: 16,
+                                                color: Colors.white))),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.only(left: 17),
+                                    child: Row(
+                                      crossAxisAlignment: CrossAxisAlignment.center,
+                                      children: [
+                                        const Icon(Icons.star, color: Colors.white, size: 14,),
+                                        const SizedBox(width: 5,),
+                                        Padding(
+                                          padding: const EdgeInsets.only(top: 2),
+                                          child: Text(texts[index][1], style: GoogleFonts.nunito(fontWeight: FontWeight.w700, fontSize: 10, color: Colors.white)),
+                                        ),
+                                      ],
+                                    ),
+                                  )
+                                ],
+                              )
+                            ],
+                          ),
                         );
                       }),
-      ),
-              Image.asset(images[4]),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 27, right: 32),
+                child: Stack(
+                  children: [
+                    Image.asset(images[4], width: double.infinity, fit: BoxFit.fill,),
+                    Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(
+                              right: 14, top: 8),
+                          child: Align(
+                            alignment: Alignment.topRight,
+                            child: Container(
+                                width: 30,
+                                height: 30,
+                                decoration: BoxDecoration(
+                                    color: Colors.black,
+                                    borderRadius:
+                                    BorderRadius.circular(30)),
+                                child: IconButton(
+                                    padding: EdgeInsets.zero,
+                                    onPressed: () {},
+                                    icon: const Icon(
+                                      Icons.favorite,
+                                      color: Colors.white,
+                                      size: 18,
+                                    ))),
+                          ),
+                        ),
+                        const SizedBox(height:  45),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 17),
+                          child: Align(
+                              alignment: Alignment.bottomLeft,
+                              child: Text("Kerela",
+                                  style: GoogleFonts.nunito(
+                                      fontWeight: FontWeight.w700,
+                                      fontSize: 16,
+                                      color: Colors.white))),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 17),
+                          child: Row(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              const Icon(Icons.star, color: Colors.white, size: 14,),
+                              const SizedBox(width: 5,),
+                              Padding(
+                                padding: const EdgeInsets.only(top: 2),
+                                child: Text("4.7", style: GoogleFonts.nunito(fontWeight: FontWeight.w700, fontSize: 10, color: Colors.white)),
+                              ),
+                            ],
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                )
+              ),
+              const SizedBox(height: 15),
               Row(
-                children: [Image.asset(images[5]), Image.asset(images[6])],
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    width: 180,
+                    child: Stack(
+                      children: [
+                        Image.asset(images[5], width: 180, fit: BoxFit.fill,),
+                        Column(
+                          children: [
+                            SizedBox(
+                              width: 180,
+                              child: Row(
+                                children: [
+                                  const Spacer(),
+                                  Padding(
+                                    padding: const EdgeInsets.only(
+                                        right: 14, top: 8),
+                                    child: Align(
+                                      alignment: Alignment.topRight,
+                                      child: Container(
+                                          width: 30,
+                                          height: 30,
+                                          decoration: BoxDecoration(
+                                              color: Colors.black,
+                                              borderRadius:
+                                              BorderRadius.circular(30)),
+                                          child: IconButton(
+                                              padding: EdgeInsets.zero,
+                                              onPressed: () {},
+                                              icon: const Icon(
+                                                Icons.favorite,
+                                                color: Colors.white,
+                                                size: 18,
+                                              ))),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            const SizedBox(height:  45),
+                            Padding(
+                              padding: const EdgeInsets.only(left: 17),
+                              child: Align(
+                                  alignment: Alignment.bottomLeft,
+                                  child: Text("Dubai",
+                                      style: GoogleFonts.nunito(
+                                          fontWeight: FontWeight.w700,
+                                          fontSize: 16,
+                                          color: Colors.white))),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(left: 17),
+                              child: Row(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  const Icon(Icons.star, color: Colors.white, size: 14,),
+                                  const SizedBox(width: 5,),
+                                  Padding(
+                                    padding: const EdgeInsets.only(top: 2),
+                                    child: Text("4.8", style: GoogleFonts.nunito(fontWeight: FontWeight.w700, fontSize: 10, color: Colors.white)),
+                                  ),
+                                ],
+                              ),
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                  const SizedBox(width: 15,),
+                  SizedBox(
+                    width: 160,
+                    child: Stack(
+                      children: [
+                        Image.asset(images[6], width: 160, fit: BoxFit.fill,),
+                        Column(
+                          children: [
+                            SizedBox(
+                              width: 160,
+                              child: Row(
+                                children: [
+                                  const Spacer(),
+                                  Padding(
+                                    padding: const EdgeInsets.only(
+                                        right: 14, top: 8),
+                                    child: Align(
+                                      alignment: Alignment.topRight,
+                                      child: Container(
+                                          width: 30,
+                                          height: 30,
+                                          decoration: BoxDecoration(
+                                              color: Colors.black,
+                                              borderRadius:
+                                              BorderRadius.circular(30)),
+                                          child: IconButton(
+                                              padding: EdgeInsets.zero,
+                                              onPressed: () {},
+                                              icon: const Icon(
+                                                Icons.favorite,
+                                                color: Colors.white,
+                                                size: 18,
+                                              ))),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            const SizedBox(height:  45),
+                            Padding(
+                              padding: const EdgeInsets.only(left: 17),
+                              child: Align(
+                                  alignment: Alignment.bottomLeft,
+                                  child: Text("Costwolds",
+                                      style: GoogleFonts.nunito(
+                                          fontWeight: FontWeight.w700,
+                                          fontSize: 16,
+                                          color: Colors.white))),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(left: 17),
+                              child: Row(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  const Icon(Icons.star, color: Colors.white, size: 14,),
+                                  const SizedBox(width: 5,),
+                                  Padding(
+                                    padding: const EdgeInsets.only(top: 2),
+                                    child: Text("4.9", style: GoogleFonts.nunito(fontWeight: FontWeight.w700, fontSize: 10, color: Colors.white)),
+                                  ),
+                                ],
+                              ),
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                ],
               )
             ],
           ),
